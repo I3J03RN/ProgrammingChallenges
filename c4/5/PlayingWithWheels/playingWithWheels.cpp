e9c74db758435d1635064e972d2e2748b42ed03e
I3J03RN <i3j03rn@outlook.com>
2019-06-30 12:14:06 +0200

    finished playing with wheels

--- c4/5/PlayingWithWheels/playingWithWheels.cpp
+++ c4/5/PlayingWithWheels/playingWithWheels.cpp
@@ -4,44 +4,62 @@
 
 using namespace std;
 
+bool visited[10][10][10][10];
+
 struct config {
     union {
         struct {
-            uint8_t _1;
-            uint8_t _2;
-            uint8_t _3;
-            uint8_t _4;
+            short _1;
+            short _2;
+            short _3;
+            short _4;
         };
-        uint32_t full;
+        long full;
     };
 
-    int getTen() { return _1 * 1000 + _2 * 100 + _3 * 10 + _4; }
+    void setVisited() { visited[_1][_2][_3][_4] = true; }
+    bool getVisited() { return visited[_1][_2][_3][_4]; }
 };
+
+ostream& operator<<(ostream& os, const config& config) {
+    os << config._1 << ", " << config._2 << ", " << config._3 << ", "
+       << config._4;
+    return os;
+}
 
 struct bfsstate {
     config _config;
     int _depth;
 };
 
-bool visited[10000] = {false};
+ostream& operator<<(ostream& os, const bfsstate& bfsstate) {
+    os << "config: " << bfsstate._config << ", depth: " << bfsstate._depth;
+    return os;
+}
 
 int main() {
     int numberofproblems;
-    while (cin >> numberofproblems) {
-        cin;
+    cin >> numberofproblems;
+    // cout << numberofproblems << "\n";
+    while (numberofproblems--) {
         config init, end;
         cin >> init._1 >> init._2 >> init._3 >> init._4;
         cin >> end._1 >> end._2 >> end._3 >> end._4;
 
-        memset(visited, false, sizeof visited);
+        // cout << "start: " << init << "\nend: " << end << "\n";
+        memset(visited, false, 10000);
 
         int numberForbidden;
         cin >> numberForbidden;
+        // cout << numberForbidden << "\n";
         while (numberForbidden--) {
             config forbidden;
             cin >> forbidden._1 >> forbidden._2 >> forbidden._3 >> forbidden._4;
-            visited[forbidden.getTen()] = true;
+            // cout << "forbidden: " << forbidden << "\n";
+            forbidden.setVisited();
         }
+
+        init.setVisited();
 
         deque<bfsstate> bfsqueue;
 
@@ -50,10 +68,64 @@ int main() {
         bool found = false;
         while (!bfsqueue.empty()) {
             bfsstate current = bfsqueue.front();
+            // cout << current << "\n";
+            bfsqueue.pop_front();
             if (current._config.full == end.full) {
                 cout << current._depth << "\n";
                 found = true;
                 break;
+            }
+            config newConfig = current._config;
+            newConfig._1 = newConfig._1 == 9 ? 0 : newConfig._1 + 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._1 = current._config._1;
+            newConfig._1 = newConfig._1 == 0 ? 9 : newConfig._1 - 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._1 = current._config._1;
+
+            newConfig._2 = newConfig._2 == 9 ? 0 : newConfig._2 + 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._2 = current._config._2;
+            newConfig._2 = newConfig._2 == 0 ? 9 : newConfig._2 - 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._2 = current._config._2;
+
+            newConfig._3 = newConfig._3 == 9 ? 0 : newConfig._3 + 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+
+            newConfig._3 = current._config._3;
+            newConfig._3 = newConfig._3 == 0 ? 9 : newConfig._3 - 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._3 = current._config._3;
+
+            newConfig._4 = newConfig._4 == 9 ? 0 : newConfig._4 + 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
+            }
+            newConfig._4 = current._config._4;
+            newConfig._4 = newConfig._4 == 0 ? 9 : newConfig._4 - 1;
+            if (!newConfig.getVisited()) {
+                bfsqueue.push_back({newConfig, current._depth + 1});
+                newConfig.setVisited();
             }
         }
         if (!found) {
