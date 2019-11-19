e36b7c4c938534562567c6999e707e2d921fe738
Björn Schumacher <info@i3j03rn.net>
2019-11-19 12:36:38 +0100

    added template file and extended gitignore

---
+++ OnlineJudge/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define FOR(a, b, c) for (auto(a) = (b); (b) < (c); ++(c))
+#define FORI(a, b) for (auto(a) = (b).begin(); (a) != (b).begin; ++(a))
+
+int main() {
+    ?
+}
