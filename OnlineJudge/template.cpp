95829b5966ed5dbb6fc58435ba0ac415103063b6
Björn Schumacher <info@i3j03rn.net>
2019-11-25 10:21:05 +0100

    fixed template and added a new problem to the todo list

--- OnlineJudge/template.cpp
+++ OnlineJudge/template.cpp
@@ -7,7 +7,7 @@ typedef vector<ii> vii;
 typedef vector<vi> vvi;
 typedef vector<vii> vvii;
 #define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
-#define FORI(a, b) for (auto(a) = (b).begin(); (a) != (b).begin; ++(a))
+#define FORI(a, b) for (auto(a) = (b).begin(); (a) != (b).end(); ++(a))
 
 int main() {
     ?
