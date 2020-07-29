a8f8de0557ba7d084fe7b2d02bbfc4ecb096e5e5
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-07-29 22:11:40 +0200

    base files

---
+++ main.cc
@@ -0,0 +1,9 @@
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cout.tie(0); cin.tie(0);
+
+  @
+  
+  return 0;
+}
---
+++ maintestcases.cc
@@ -0,0 +1,18 @@
+
+void solve() {
+  @
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cout.tie(0); cin.tie(0);
+
+  int tt = 1;
+  cin >> tt;
+  FOR (t, 1, tt + 1) {
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+  
+  return 0;
+}
