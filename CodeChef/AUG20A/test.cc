f81b64cc5bc6f85a52395f270f302823252c03e1
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-12-03 09:23:41 +0100

    aug20

---
+++ CodeChef/AUG20A/test.cc
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define SZ(x) (int)(x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto a = (b); (a) < (c); ++(a))
+#define F0R(a, b) FOR (a, 0, (b))
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+  if (DEBUG) std::cerr
+#define dvar(...) " [" << #__VA_ARGS__ ": " << mt(__VA_ARGS__) << "] "
+template <typename T>
+struct IsC {
+  template <typename C>
+  static char test(typename C::const_iterator*);
+  template <typename C>
+  static int test(...);
+  static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+template <>
+struct IsC<string> {
+  static const bool value = false;
+};
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(ostream&, const C&);
+template <typename T1, typename T2>
+ostream& operator<<(ostream&, const pair<T1, T2>&);
+template <typename... T> using TS = tuple_size<tuple<T...>>;
+template<size_t idx, typename... T>
+typename enable_if<TS<T...>::value == idx + 1, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return o << ", " << get<idx>(t) << ')';
+}
+template<size_t idx, typename... T>
+typename enable_if<0 < idx && idx + 1 < TS<T...>::value, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << ", " << get<idx>(t), t);
+}
+template<size_t idx = 0, typename... T>
+typename enable_if<1 < TS<T...>::value && !idx, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << '(' << get<idx>(t), t);
+}
+template<typename T>
+ostream& operator<<(ostream& o, const tuple<T>& t) {
+  return o << get<0>(t);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& o, const pair<T1, T2>& p) {
+  return o << mt(p.fi, p.se);
+}
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(
+    ostream& o, const C& c) {
+  o << '[';
+  for (auto it = c.cbegin(); it != c.cend(); ++it)
+    o << *it << (next(it) != c.cend() ? ", " : "");
+  return o << ']';
+}
+template <typename T>
+void tprint(vector<vector<T>>& v, size_t width = 0, ostream& o = cerr) {
+  if (!DEBUG) return;
+  for (const auto& vv : v) {
+    for (const auto& i : vv) o << setw(width) << i;
+    o << endl;
+  }
+}
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+struct SuffixArray {
+	vi sa, lcp;
+	SuffixArray(string& s, int lim=256) { // or basic_string<int>
+		int n = sz(s) + 1, k = 0, a, b;
+		vi x(all(s)+1), y(n), ws(max(n, lim)), rank(n);
+		sa = lcp = y, iota(all(sa), 0);
+		for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
+			p = j, iota(all(y), n - j);
+			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
+			fill(all(ws), 0);
+			rep(i,0,n) ws[x[i]]++;
+			rep(i,1,lim) ws[i] += ws[i - 1];
+			for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+			swap(x, y), p = 1, x[sa[0]] = 0;
+			rep(i,1,n) a = sa[i - 1], b = sa[i], x[b] =
+				(y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+		}
+		rep(i,1,n) rank[sa[i]] = i;
+		for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+			for (k && k--, j = sa[rank[i] - 1];
+					s[i + k] == s[j + k]; k++);
+	}
+};
+
+void solve() {
+  string s; cin >> s;
+  cout << s.at(sz(s)) << endl;
+  SuffixArray sa(s);
+  cout << dvar(sa.sa, sa.lcp) << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cout.tie(0); cin.tie(0);
+
+  int tt = 1;
+  FOR (t, 1, tt + 1) {
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+  
+  return 0;
+}
