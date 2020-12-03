d152ab4ddd9413c98545baafdb71309d709dd0e3
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-12-03 09:46:32 +0100

    AoC 2020 1-3

--- main.cc
+++ main.cc
@@ -1,7 +1,17 @@
+template<typename F>
+vector<string> input(F f) {
+  vector<string> ss;
+  for (string s; getline(cin, s); ss.pb(s)) {
+    stringstream inp(s);
+    f(inp);
+  }
+  return ss;
+}
+
 
 int main() {
   ios_base::sync_with_stdio(0);
-  cout.tie(0); cin.tie(0);
+  cin.tie(0);
 
   @
   
--- maintestcases.cc
+++ maintestcases.cc
@@ -5,7 +5,7 @@ void solve() {
 
 int main() {
   ios_base::sync_with_stdio(0);
-  cout.tie(0); cin.tie(0);
+  cin.tie(0);
 
   int tt = 1;
   cin >> tt;
