e0fe0240520ea8933ce232b8a197a977a1c2a505
Björn Schumacher <stu214530@mail.uni-kiel.de>
2021-01-09 15:53:49 +0100

    Codeforces Round 694 (Div. 1) A, B and D

--- main.cc
+++ main.cc
@@ -1,13 +1,3 @@
-template<typename F>
-vector<string> input(F f) {
-  vector<string> ss;
-  for (string s; getline(cin, s); ss.pb(s)) {
-    stringstream inp(s);
-    f(inp);
-  }
-  return ss;
-}
-
 
 int main() {
   ios_base::sync_with_stdio(0);
